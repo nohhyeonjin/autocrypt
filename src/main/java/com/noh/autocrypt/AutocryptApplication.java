28f1f373c4f4dd7822dc108704fb7f1430f49f2c
Noh Hyeon Jin <hjforj@naver.com>
2022-07-08 14:33:43 +0900

    initial commit

---
+++ src/main/java/com/noh/autocrypt/AutocryptApplication.java
@@ -0,0 +1,13 @@
+package com.noh.autocrypt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AutocryptApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AutocryptApplication.class, args);
+    }
+
+}
