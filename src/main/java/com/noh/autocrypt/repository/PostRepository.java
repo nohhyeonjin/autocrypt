84edb9cb0ef662011dfae58affb726c456b0e006
Noh Hyeon Jin <hjforj@naver.com>
2022-07-09 16:19:54 +0900

    refactor: 불필요하게 선언된 함수 제거

--- src/main/java/com/noh/autocrypt/repository/PostRepository.java
+++ src/main/java/com/noh/autocrypt/repository/PostRepository.java
@@ -4,11 +4,7 @@ import com.noh.autocrypt.domain.Member;
 import com.noh.autocrypt.domain.Post;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.util.List;
-
 public interface PostRepository extends JpaRepository<Post, Long> {
-
-    Post save(Post post);
 
     Post findByMemberAndId(Member member, Long postId);
 
