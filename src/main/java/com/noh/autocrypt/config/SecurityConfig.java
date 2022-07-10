db5ccd13574c90d559a87c74d0fa20415744a488
Noh Hyeon Jin <hjforj@naver.com>
2022-07-10 12:13:34 +0900

    fix: CORS filter 추가

--- src/main/java/com/noh/autocrypt/config/SecurityConfig.java
+++ src/main/java/com/noh/autocrypt/config/SecurityConfig.java
@@ -1,5 +1,6 @@
 package com.noh.autocrypt.config;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -7,11 +8,15 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 
 @Configuration
 @EnableWebSecurity
+@RequiredArgsConstructor
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    private final CorsConfig corsConfig;
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.csrf().disable();
+        http.addFilter(corsConfig.corsFilter());
         http.formLogin()
                 .loginProcessingUrl("/login")
                 .usernameParameter("email")
